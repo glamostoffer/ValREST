3e5d830fecd2867d0f1b547d2fa128b5bfa1daa0
glamostoffer <glamostoffer@gmail.com>
2023-04-13 21:37:37 +0300

    Added user and lineup models

---
+++ internal/models/lineup.go
@@ -0,0 +1,9 @@
+package models
+
+type Lineup struct {
+	ID        int    `json:"id"`
+	Agent     string `json:"agent"`
+	MapName   string `json:"map"`
+	Objective bool   `json:"objective"`
+	Ability   string `json:"ability"`
+}
