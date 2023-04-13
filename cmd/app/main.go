c1b7ce24cb5e9ec32e3a7fb31e41a3a44819b149
glamostoffer <glamostoffer@gmail.com>
2023-04-13 23:16:55 +0300

    Added initialization config; used viper

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,15 +6,27 @@ import (
 	"ValREST/internal/handlers"
 	"ValREST/internal/services"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repo.NewRepository()
 	services := services.NewService(repos)
 	handler := handlers.NewHandler(services)
 
 	serv := new(app.Server)
-	if err := serv.Start(handler.InitRouts()); err != nil {
+	if err := serv.Start(viper.GetString("port"), handler.InitRouts()); err != nil {
 		log.Fatalf("error starting server: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -10,9 +10,9 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Start(handler http.Handler) error {
+func (s *Server) Start(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + port,
 		Handler:        handler,
 		MaxHeaderBytes: 1 << 20, // 1 mb
 		ReadTimeout:    10 * time.Second,
