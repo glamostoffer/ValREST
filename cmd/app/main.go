ea5ca1fb74deb3fd54ad841d5ccc6bb219caf1c7
glamostoffer <glamostoffer@gmail.com>
2023-04-13 21:34:21 +0300

    Added server package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	app "ValREST/internal/app"
+	"log"
+)
+
+func main() {
+	serv := new(app.Server)
+	if err := serv.Start(); err != nil {
+		log.Fatalf("error starting server: %s", err.Error())
+	}
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,26 @@
 package app
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Start() error {
+	s.httpServer = &http.Server{
+		Addr:           ":8080",
+		MaxHeaderBytes: 1 << 20, // 1 mb
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
