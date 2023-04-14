f46f4423359fff6f9bfc71d9ffd877f1cc2ad30e
glamostoffer <glamostoffer@gmail.com>
2023-04-14 19:33:20 +0300

    Started work on registry

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,8 +1,20 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"ValREST/internal/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
 
 func (h *Handler) register(c *gin.Context) {
+	var input models.User
+
+	if err := c.BindJSON(&input); err != nil {
+		logrus.Errorf("error binding json: %s", err.Error())
+		c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
+		return
+	}
 
 }
 
